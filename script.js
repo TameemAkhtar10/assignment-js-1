b502ee6a20007b902cae3ce34b2325151207912f
tameem akhtar <akhtartamimm@gmail.com>
2025-10-28 21:05:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+alert("i m from sheriyans")
+// according to js is mind of the body
+
+// according to me everywhwere  like animation interactivity backend
+console.log("connected!")
+
+let a = "tameem";
+var b = "sheriyans";
+const c = "school";
+a = "updated";
+b = "updated";
+// c = "updated";
+// {
+//     let tameem = "MERA NAAM HAI BHAI  YE";
+// }
+// console.log(tameem)
+const meranaam = "tameem akhtar";
+const adharnmbr = 430261315067;
+const everything = "merii ammii";
+
+
+let name = "tameem akhtar";
+let age = 18;
+let city = "nagpur";
+console.log(name);
+console.warn(age);
+console.info(city);
+
+
+alert("welcome");
+let t10 = prompt("my name is ")
+console.log(t10);
+
+
+let ages = prompt("enter u r age");
+console.log((ages + 5));
+
+//m jaise hi plus krrra hu to wo concatinate hora h string q ke js me plus srf add krne ka kam ni krta balke string k bich m concatinate bhi krta h 
+
+
+let msg = "i love sheriyans";
+console.log(msg.slice(2,6))
+console.log(msg.split(" ")); console.log(msg.replace("love","study at"))
+console.log(msg.replace("love","study at"))
+console.log(msg.includes("love"));
+let nameee = "tameem";
+console.log(`hey ${nameee} welcome to js`);
+
+//ha bhai code run hora h q k js apni glti ko chhupa leta h 
+// nhi kuch ni hora but jaise hi m semicolon hta ra hu to error arah  
+//  kuch hua to ni but i think zyda bada code raha to error to ni aayenga but bad m bht problem hongi
+
+
+
+//tameem akhtar
+//bhai y jo hai comment h isse hamko kisi devloper ko kuch btana rhta h to ham bta dete h 
+// nhi bhai output ni aaara h q k comment readable ni hota compiler k liye
+
+
+// expression direct value deta h bhai 
+let y = (10+5)*2;
+console.log(y);
+
+//or isme bodmass use hua h bhai
+
+
+let umar = 19;
+let naam = "TAMEEM";
+let isstudent = true;
+let skilll = ["js","css3"];
+let user = {
+    city:"nagpur"
+    
+
+}
+let x = null;
+let n;
+let z =Symbol("id");
+
+console.log(typeof umar);
+console.log(typeof naam);
+console.log(typeof isstudent);
+console.log(typeof skilll);
+console.log(typeof user);
+console.log(typeof x);
+console.log(typeof n);
+console.log(typeof z);
+
+// dekh bhai array ka bhi object dikhara h y problem h or null ka bhi mujhe yad h ap y class m pdaye the
+
+
+
+console.log(1/0);
+console.log(0/0);
+console.log(Number("bac"));
+let taa;
+console.log(taa +1);
+
+// ek me infinity aara h baaki sb m NaN
+
+let mai = 5;
+ let ya = mai;
+  ya = 10; 
+  console.log(mai, ya);
+//   iski value aasani s copy ho jati h 
+
+
+  let obj1 = { name:"Harsh" };
+let obj2 = obj1;
+obj2.name= "sheriyans";
+console.log(obj1);
+// sheriyans print hua h object m q k array object and function ki value copy hoti h magar aisa hi issue aata h git status
